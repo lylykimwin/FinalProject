e452eb955cfdb0467a52df0ffe68246aa06e1081
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-12-02 18:14:45 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+<h1>Welcome to Lyly's Library</h1>
+<p>Explore the virtual library with detailed information about books, authors, and their prices. Dive into the data behind some books!</p>
+<?php
+include "view-footer.php";
+?>
