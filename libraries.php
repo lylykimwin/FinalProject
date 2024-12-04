e9743715749196831a51a6f06693ba9191ebd92c
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-12-04 03:15:44 -0600

    Create libraries.php

---
+++ libraries.php
@@ -0,0 +1,9 @@
+<?php
+// CSS Libraries
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">';
+echo '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">';
+
+// JavaScript Libraries
+echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>';
+echo '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>';
+?>
