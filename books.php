e6d768f2e6e77dd7911503ddd3e1f6a133d1be83
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-12-04 13:48:13 -0600

    Update books.php

--- books.php
+++ books.php
@@ -112,6 +112,4 @@ $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-"""
 
-books_php_adjusted
